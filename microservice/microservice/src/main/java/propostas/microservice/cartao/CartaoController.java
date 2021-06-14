cf3e88ed907013aa04a94fe31e81181bbe86b29d
Werliney <werli17@hotmail.com>
2021-06-14 18:06:18 -0300

    Funcionalidade de login via password

--- microservice/microservice/src/main/java/propostas/microservice/cartao/CartaoController.java
+++ microservice/microservice/src/main/java/propostas/microservice/cartao/CartaoController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
